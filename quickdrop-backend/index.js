d3368bd65b70969664440c2b151a885f2fc282a8
Ranjan <ranjan.288.bej@gmail.com>
2025-06-21 15:36:21 +0530

    updated index.js

--- quickdrop-backend/index.js
+++ quickdrop-backend/index.js
@@ -18,7 +18,11 @@ const storage = multer.diskStorage({
 })
 Connect();
 const upload = multer({ storage:storage })
-app.use(cors())
+app.use(cors({
+    origin:[],
+    methods:["POST","GET"],
+    credentials:true
+}))
 app.use(express.json())
 // app.use(express.urlencoded({ extended: true }))
 const port = process.env.PORT
