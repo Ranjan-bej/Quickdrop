5e6a4b7ae5a9cd0703545e23478c2bc381656b69
Ranjan <ranjan.288.bej@gmail.com>
2025-06-21 17:18:16 +0530

    added cors info

--- quickdrop-backend/index.js
+++ quickdrop-backend/index.js
@@ -18,7 +18,9 @@ const storage = multer.diskStorage({
 })
 Connect();
 const upload = multer({ storage:storage })
-app.use(cors())
+app.use(cors({
+    origin:["https://quickdrop-chi.vercel.app"]
+}))
 app.use(express.json())
 // app.use(express.urlencoded({ extended: true }))
 const port = process.env.PORT
